e07cb5332e4a96eb7a9cec1f06be3f34927c84e9
Max Manayan <maximillian521@gmail.com>
2021-05-24 14:19:25 -0500

    lessons 13-14 complete

--- section2-fundamentals/lesson09/script.js
+++ section2-fundamentals/lesson09/script.js
@@ -11,12 +11,46 @@
 
 // console.log(`${country}, ${continent}: ${population}`)
 
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun)
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun)
 
-console.log(typeof true)
+// console.log(typeof true)
 
-// CHANGING VALUE OF VARIABLE
-javaScriptIsFun = "YES!!!"
+// // CHANGING VALUE OF VARIABLE
+// javaScriptIsFun = "YES!!!"
 
-console.log(javaScriptIsFun)
+// console.log(javaScriptIsFun)
+
+// let age = 24
+// age = 25
+
+// const birthYear = 1997
+
+// var job = "teacher"
+// job = "programmer"
+
+// MATH OPERATORS
+const now = 2037;
+const ageMax = now - 1997;
+const ageJin = now - 1996;
+console.log(ageMax, ageJin);
+
+console.log(ageMax + 2, ageJin / 10, 2 ** 3)
+
+const firstName = 'Max';
+const lastName = 'Manayan';
+console.log(firstName + ' ' + lastName)
+
+// ASSIGNMENT OPERATORS
+let x = 10 + 5 // 15
+x += 10 // 25
+x *= 4 //100
+x++ // x = x + 1
+x-- // x = x -1
+console.log(x);
+
+//COMPARISON OPERATORS
+console.log(ageJin > ageMax)
+console.log(ageJin <= 18)
+
+const isFullAge = ageJin >= 18
