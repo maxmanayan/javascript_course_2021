2ec68357bad8d3b8994532d5ca160cc44bea79d0
Max Manayan <maximillian521@gmail.com>
2021-05-24 13:52:06 -0500

    lesson 10-12 complete

--- section2-fundamentals/lesson09/script.js
+++ section2-fundamentals/lesson09/script.js
@@ -1,5 +1,22 @@
-let js = 'amazing';
- 
-if (js==='amazing') alert("JavaScript is AMAZING")
+// let js = 'amazing';
 
-console.log(40+35+10)
+// console.log(40+35+10);
+
+// let firstName = "Max";
+// console.log(firstName);
+
+// let country = "USA"
+// let continent = "North America"
+// let population = "330 million"
+
+// console.log(`${country}, ${continent}: ${population}`)
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun)
+
+console.log(typeof true)
+
+// CHANGING VALUE OF VARIABLE
+javaScriptIsFun = "YES!!!"
+
+console.log(javaScriptIsFun)
