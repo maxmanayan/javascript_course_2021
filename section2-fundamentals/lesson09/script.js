a57c58f06d675e928f582fbff50a071dc2f2d848
Max Manayan <maximillian521@gmail.com>
2021-05-24 14:42:17 -0500

    lesson 16 complete

--- section2-fundamentals/lesson09/script.js
+++ section2-fundamentals/lesson09/script.js
@@ -29,28 +29,58 @@
 // var job = "teacher"
 // job = "programmer"
 
-// MATH OPERATORS
-const now = 2037;
-const ageMax = now - 1997;
-const ageJin = now - 1996;
-console.log(ageMax, ageJin);
+// // MATH OPERATORS
+// const now = 2037;
+// const ageMax = now - 1997;
+// const ageJin = now - 1996;
+// console.log(ageMax, ageJin);
 
-console.log(ageMax + 2, ageJin / 10, 2 ** 3)
+// console.log(ageMax + 2, ageJin / 10, 2 ** 3)
 
-const firstName = 'Max';
-const lastName = 'Manayan';
-console.log(firstName + ' ' + lastName)
+// const firstName = 'Max';
+// const lastName = 'Manayan';
+// console.log(firstName + ' ' + lastName)
 
-// ASSIGNMENT OPERATORS
-let x = 10 + 5 // 15
-x += 10 // 25
-x *= 4 //100
-x++ // x = x + 1
-x-- // x = x -1
-console.log(x);
+// // ASSIGNMENT OPERATORS
+// let x = 10 + 5 // 15
+// x += 10 // 25
+// x *= 4 //100
+// x++ // x = x + 1
+// x-- // x = x -1
+// console.log(x);
 
-//COMPARISON OPERATORS
-console.log(ageJin > ageMax)
-console.log(ageJin <= 18)
+// //COMPARISON OPERATORS
+// console.log(ageJin > ageMax)
+// console.log(ageJin <= 18)
 
-const isFullAge = ageJin >= 18
+// const isFullAge = ageJin >= 18
+
+
+// CODING CHALLENGE #1
+
+// let markMass = 78
+// let markHeight = 1.69
+// let johnMass = 92
+// let johnHeight = 1.95
+
+// let markBMI = markMass / (markHeight * markHeight)
+// let johnBMI = johnMass / (johnHeight * johnHeight)
+
+// console.log(markBMI, johnBMI)
+
+// let markHigherBMI = markBMI > johnBMI
+// console.log(markHigherBMI)
+
+// markMass = 95
+// markHeight = 1.88
+// johnMass = 85
+// johnHeight = 1.76
+
+
+// markBMI = markMass / (markHeight * markHeight)
+// johnBMI = johnMass / (johnHeight * johnHeight)
+
+// console.log(markBMI, johnBMI)
+
+// markHigherBMI = markBMI > johnBMI
+// console.log(markHigherBMI)
