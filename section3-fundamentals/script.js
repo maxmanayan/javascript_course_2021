8239f587ee98796544baa9ad4e2c73147084a04c
Max Manayan <maximillian521@gmail.com>
2021-06-20 14:56:09 -0500

    section 3 started

---
+++ section3-fundamentals/script.js
@@ -0,0 +1,7 @@
+// strict mode can catch errors and give warnings
+'use strict';
+
+// let hasDriversLicense = false
+// let passedTest = true 
+// if (passedTest) hasDriversLicense = true 
+// if (hasDriversLicense) console.log('I have a drivers license.')
