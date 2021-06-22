a3b542d8030ee61b64bdc6e6fc49781db57e01d8
Max Manayan <maximillian521@gmail.com>
2021-06-22 18:25:50 -0500

    lesson 36 complete

--- section3-fundamentals/script.js
+++ section3-fundamentals/script.js
@@ -46,3 +46,18 @@
 // }
 
 // console.log(yearsUntilRetirement(1997, 'Max'))
+
+// // LESSON 36 - Calling a function with another function
+// function cutFruitPieces(fruit) {
+//   return fruit * 4
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples)
+//   const orangePieces = cutFruitPieces(oranges)
+//   const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`
+//   return juice
+// }
+
+// console.log(fruitProcessor(3,4))
+
