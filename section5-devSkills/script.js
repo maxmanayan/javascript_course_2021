01af2aed15524b9900a07e675263ed7204a3120e
Max Manayan <maximillian521@gmail.com>
2021-06-26 13:25:07 -0500

    lesson 55 complete - installed Prettier

--- section5-devSkills/script.js
+++ section5-devSkills/script.js
@@ -1,2 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+
+// Lesson 55 - Installing Prettier and vscode snippets
+const x = 23;
+
+if (x === 23) console.log("MJ");
+
+const calcAge = (birthYear) => 23;
+
+// installed Prettier
