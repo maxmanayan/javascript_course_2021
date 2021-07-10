0634d029cfb27aac96cea52820b25b3f1a306b41
Max Manayan <maximillian521@gmail.com>
2021-07-10 13:58:48 -0600

    lesson 72 complete

--- section7-dom-events/script.js
+++ section7-dom-events/script.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
+
+// // changing the HTML message w/ JS
+// document.querySelector(".message").textContent = "Correct Number!";
+
+// // changing the HTML number and score w/ JS
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 19;
+
+// console.log(document.querySelector(".guess").value);
+// document.querySelector(".guess").value = 23;
+
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+});
