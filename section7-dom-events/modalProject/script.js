81e6dcea5f129e594ba98c90748b988602b9a1c8
Max Manayan <maximillian521@gmail.com>
2021-07-12 14:27:09 -0600

    lesson 81 complete, modal project complete

--- section7-dom-events/modalProject/script.js
+++ section7-dom-events/modalProject/script.js
@@ -1,10 +1,12 @@
 "use strict";
 
+// assigning variables to html classes
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
+// creating functions to aggregate modal events
 const openModal = () => {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
@@ -15,9 +17,16 @@ const closeModal = () => {
   overlay.classList.add("hidden");
 };
 
+// opens modal when buttons are clicked
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", openModal);
 }
 
+// closes modal when button or overlay is clicked
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+// closes modal when "ESC" key is pressed
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
