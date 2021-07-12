b951426bc6759df77f03deab99f842c867ca13a0
Max Manayan <maximillian521@gmail.com>
2021-07-12 13:56:29 -0600

    lesson 80 complete

--- section7-dom-events/modalProject/script.js
+++ section7-dom-events/modalProject/script.js
@@ -5,6 +5,19 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i]);
+  btnsOpenModal[i].addEventListener("click", openModal);
 }
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
