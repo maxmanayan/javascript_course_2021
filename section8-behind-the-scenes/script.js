44911f5e42160b43c27247bb98452f24635513cd
Max Manayan <maximillian521@gmail.com>
2021-07-21 11:09:48 -0600

    lesson 93 complete

--- section8-behind-the-scenes/script.js
+++ section8-behind-the-scenes/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+function calcAge(birthYear) {
+  const age = 2021 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}!`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = "Jin";
+
+      // Reassigning outer scope's variable
+      output = "NEW OUTPUT!!!";
+
+      const str = `Oh, and you're a millenial, ${firstName}!`;
+      console.log(str);
+    }
+
+    console.log(millenial);
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = "Max";
+calcAge(1996);
