05beb0961353cf6a4df0b2ddf48fe65d4c4826b7
Max Manayan <maximillian521@gmail.com>
2021-07-21 11:49:33 -0600

    lesson 95 complete

--- section8-behind-the-scenes/script.js
+++ section8-behind-the-scenes/script.js
@@ -1,32 +1,76 @@
 "use strict";
 
-function calcAge(birthYear) {
-  const age = 2021 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2021 - birthYear;
 
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}!`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}!`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
 
-      // Creating NEW variable with same name as outer scope's variable
-      const firstName = "Jin";
+//       // Creating NEW variable with same name as outer scope's variable
+//       const firstName = "Jin";
 
-      // Reassigning outer scope's variable
-      output = "NEW OUTPUT!!!";
+//       // Reassigning outer scope's variable
+//       output = "NEW OUTPUT!!!";
 
-      const str = `Oh, and you're a millenial, ${firstName}!`;
-      console.log(str);
-    }
+//       const str = `Oh, and you're a millenial, ${firstName}!`;
+//       console.log(str);
+//     }
 
-    console.log(millenial);
-    console.log(output);
-  }
-  printAge();
+//     console.log(millenial);
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstName = "Max";
+// calcAge(1996);
+
+// Lesson 95
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Max";
+let job = "engineer";
+const year = 1991;
+
+// Functions
+
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = "Max";
-calcAge(1996);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Hoisting Pitfall Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
