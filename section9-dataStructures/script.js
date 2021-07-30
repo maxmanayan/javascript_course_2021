9f95c89f1ae2eaa75b1d7c1448f890f6b60a5506
Max Manayan <maximillian521@gmail.com>
2021-07-30 11:48:23 -0600

    lesson 114 complete

--- section9-dataStructures/script.js
+++ section9-dataStructures/script.js
@@ -430,3 +430,60 @@ const game = {
 // for (const [key, { open, close }] of entries) {
 //   console.log(`On ${key}, we open at ${open} and close at ${close}!`);
 // }
+
+// // Lesson 114 - Coding Challenge #2
+// /*
+// Let's continue with our football betting app!
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+//       Odd of victory Bayern Munich: 1.33
+//       Odd of draw: 3.25
+//       Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+// BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+//       {
+//         Gnarby: 1,
+//         Hummels: 1,
+//         Lewandowski: 2
+//       }
+// GOOD LUCK 😀
+// */
+
+// // 1.
+// for (const [i, playerName] of game.scored.entries()) {
+//   console.log(`Goal ${i + 1}: ${playerName}`);
+// }
+
+// // 2.
+// let oddsValues = Object.values(game.odds);
+
+// let average =
+//   oddsValues.reduce((acc, cur) => {
+//     return acc + cur;
+//   }) / oddsValues.length;
+
+// console.log(average);
+
+// // 3.
+// let oddsEntries = Object.entries(game.odds);
+// console.log(oddsEntries);
+
+// for (const [team, odd] of oddsEntries) {
+//   console.log(
+//     `Odd of ${game[team] ? `victory ${game[team]}` : "draw"}: ${odd}`
+//   );
+// }
+
+// // Bonus:
+// let scorers = {};
+
+// for (const [i, player] of game.scored.entries()) {
+//   if (player in scorers) {
+//     scorers[player] += 1;
+//   } else {
+//     scorers[player] = 1;
+//   }
+// }
+
+// console.log(scorers);
