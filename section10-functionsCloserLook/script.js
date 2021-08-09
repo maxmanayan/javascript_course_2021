e1f9af2c702f753f964591393026e196596919db
Max Manayan <maximillian521@gmail.com>
2021-08-09 10:47:09 -0600

    lesson 128 complete

--- section10-functionsCloserLook/script.js
+++ section10-functionsCloserLook/script.js
@@ -1,31 +1,65 @@
 "use strict";
 
-// Lesson 127 - Default Parameters
+// // Lesson 127 - Default Parameters
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  // // ES5 Way
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // // ES5 Way
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
 
-  console.log(booking);
-  bookings.push(booking);
-};
+//   console.log(booking);
+//   bookings.push(booking);
+// };
 
-createBooking("LH123");
-createBooking("LH123", 2, 800);
-createBooking("LH123", 2);
-createBooking("LH123", 5);
+// createBooking("LH123");
+// createBooking("LH123", 2, 800);
+// createBooking("LH123", 2);
+// createBooking("LH123", 5);
 
-createBooking("LH123", undefined, 1000);
+// createBooking("LH123", undefined, 1000);
+
+// // Lesson 128 - How PAssing Arguments Works: Value vs. Reference
+// const flight = "LH123";
+// const max = {
+//   name: "Max Manayan",
+//   passport: 1234567890,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = "LH999";
+//   passenger.name = "Mr. " + passenger.name;
+
+//   if ((passenger.passport = 1234567890)) {
+//     alert("Checked in");
+//   } else {
+//     alert("Wrong passport");
+//   }
+// };
+
+// checkIn(flight, max);
+
+// console.log(flight);
+// console.log(max);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+//   console.log("new passport number", person);
+// };
+
+// newPassport(max);
+// checkIn(flight, max);
+
+// console.log(flight);
+// console.log(max);
