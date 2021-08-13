021d4971ed03b035a9b5114e1b74090e8865028b
Max Manayan <maximillian521@gmail.com>
2021-08-13 11:36:50 -0600

    lesson 138 (coding challenge #2) complete

--- section10-functionsCloserLook/script.js
+++ section10-functionsCloserLook/script.js
@@ -318,45 +318,62 @@ GOOD LUCK 😀
 // // how to view the closure variables
 // console.dir(booker);
 
-// Lesson 137 - More Closure Examples
-let f;
+// // Lesson 137 - More Closure Examples
+// let f;
 
-const g = function () {
-  const a = 23;
-  f = function () {
-    console.log(a * 2);
-  };
-};
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
 
-const h = function () {
-  const b = 777;
-  f = function () {
-    console.log(b * 2);
-  };
-};
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
 
-g();
-f();
-console.dir(f);
+// g();
+// f();
+// console.dir(f);
 
-// Re-assigning f function
-h();
-f();
-console.dir(f);
+// // Re-assigning f function
+// h();
+// f();
+// console.dir(f);
 
-// Example 2
-const boardPassengers = function (n, wait) {
-  const perGroup = n / 3;
+// // Example 2
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
 
-  setTimeout(function () {
-    console.log(`We are now boarding all ${n} passengers`);
-    console.log(`There are 3 groups, each with ${perGroup} passengers`);
-  }, wait * 1000);
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
 
-  console.log(`Will start boarding in ${wait} seconds`);
-};
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
 
-// Closure will have priority over the scope chain
-let perGroup = 300;
+// // Closure will have priority over the scope chain
+// let perGroup = 300;
 
-boardPassengers(240, 3);
+// boardPassengers(240, 3);
+
+// Lesson 138 - Coding Challenge #2
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
